bb0d2d8589538bfc3d590f4cfc2958e4280158e5
ungdoong <xorwls124@gmail.com>
2020-01-29 18:05:13 +0900

    [FEAT]그룹생성 UI

---
+++ front-sk/src/components/user/MyGroupList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+    class="mx-4"
+    tile
+  >
+    <v-list class="pa-4">
+      <p class="ml-4">가입한 모임 목록</p><hr/>
+      <v-list-item-group>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      item: 1,
+      items: [
+        { text: 'Real-Time', icon: 'mdi-clock' },
+        { text: 'Audience', icon: 'mdi-account' },
+        { text: 'Conversions', icon: 'mdi-flag' },
+      ],
+    }),
+  }
+</script>
+
+<style>
+p {
+  font-size: 30px;
+}
+</style>
