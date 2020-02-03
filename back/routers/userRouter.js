7ced51ad76aa0609d08d6b3027585a45f2a7c748
ungdoong <xorwls124@gmail.com>
2020-02-03 18:09:34 +0900

    2020-02-03 Axios 연동

--- back/routers/userRouter.js
+++ back/routers/userRouter.js
@@ -1,12 +1,24 @@
 import express from "express"
 import routes from "../routes"
-import { user, userDetail, changePassword, signup, signin, read_users } from "../controllers/userController";
+import { 
+    read_users, 
+    read_user, 
+    update_user, 
+    delete_user,
+    signup, 
+    signin,
+    social_signin,
+    check_token
+} from "../controllers/userController";
 
 const userRouter = express.Router();
-
-userRouter.get(routes.home, read_users);
 userRouter.post(routes.signup, signup);
 userRouter.post(routes.signin, signin);
-userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.post(routes.social_signin, social_signin);
+userRouter.post(routes.check_token, check_token);
+userRouter.get(routes.home, read_users);
+userRouter.get(routes.userDetail, read_user);
+userRouter.put(routes.userDetail, update_user);
+userRouter.delete(routes.userDetail, delete_user);
+
 export default userRouter;
--- front-sk/src/App.vue
+++ front-sk/src/App.vue
@@ -2,28 +2,14 @@
   <v-app id="app">
     <router-view name="header" />
     <v-fade-transition>
-      <router-view class="mainView" />
+      <router-view class="mainView fill-height" />
     </v-fade-transition>
     <router-view name="footer" />
   </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import WbKakao from "@/social-signin/kakao/kakao";
-
 export default {
-  methods: {
-    ...mapActions(['checkToken']),
-  },
-  async mounted() {
-    let accessToken = await localStorage.getItem("accessToken");
-    if (!accessToken) {
-      accessToken = await sessionStorage.getItem("accessToken");
-    }
 
-    this.checkToken({token:accessToken});
-    WbKakao.init();
-  }
 };
 </script>
